50baaaf6ebb0afa61fcd3c52fa307c6c60fb0c34
Kartik <kartik20044@gmail.com>
2024-09-28 19:39:10 +0530

    Gettoken function removed from midddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,38 +1,5 @@
-// import { NextResponse } from "next/server";
-// import { auth as middleware } from "@/auth";
-// import { getGitHubToken } from "./actions/get-github-token";
-
-// export default middleware(async (req) => {
-//     const { pathname, origin } = req.nextUrl;
-
-//     const allowedPaths = ["/", "/login", "http://localhost:3000/api/auth/session"];
-//     const isStaticFile = pathname.startsWith("/_next/") || pathname.startsWith("/static/") || pathname.includes(".");
-
-//     if (!req.auth && !allowedPaths.includes(pathname) && !isStaticFile) {
-//         return NextResponse.redirect(new URL("/login", origin));
-//     }
-
-//     if (req.auth && pathname === "/login") {
-//         return NextResponse.redirect(new URL("/dashboard", origin));
-//     }
-
-//     if (pathname === "/dashboard") {
-//         const token = await getGitHubToken();
-//         if (token === "NO_TOKEN") {
-//             return NextResponse.redirect(new URL("/", origin));
-//         }
-//     }
-
-//     return NextResponse.next();
-// });
-
-// export const config = {
-//     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-// };
-
 import { NextResponse } from "next/server";
 import { auth } from "@/auth";
-import { getGitHubToken } from "./actions/get-github-token";
 
 const allowedPathsRegex = /^\/($|login|api\/auth\/session)/;
 const isStaticFile = (pathname: string) => /^\/_next\/|^\/static\/|\./.test(pathname);
@@ -50,14 +17,6 @@ export default auth((req) => {
 
     if (req.auth && pathname === "/login") {
         return NextResponse.redirect(new URL("/dashboard", origin));
-    }
-
-    if (pathname === "/dashboard") {
-        return getGitHubToken().then((token) =>
-            token === "NO_TOKEN"
-                ? NextResponse.redirect(new URL("/", origin))
-                : NextResponse.next()
-        );
     }
 
     return NextResponse.next();
