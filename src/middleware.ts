45be3c21b50bee9284b233e821808fcfacee6642
Kartik <kartik20044@gmail.com>
2024-09-28 12:34:38 +0530

    middleware size and loc reduced

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,37 +3,23 @@ import { auth as middleware } from "@/auth";
 import { getGitHubToken } from "./actions/get-github-token";
 
 export default middleware(async (req) => {
-
-
-    const allowedPaths = [
-        "/",
-        "/login",
-        "http://localhost:3000/api/auth/session",
-    ];
     const { pathname, origin } = req.nextUrl;
 
-    // Check if the request is for a static file
-    const isStaticFile =
-        pathname.startsWith("/_next/") ||
-        pathname.startsWith("/static/") ||
-        pathname.includes(".");
+    const allowedPaths = ["/", "/login", "http://localhost:3000/api/auth/session"];
+    const isStaticFile = pathname.startsWith("/_next/") || pathname.startsWith("/static/") || pathname.includes(".");
 
     if (!req.auth && !allowedPaths.includes(pathname) && !isStaticFile) {
-        const newUrl = new URL("/login", origin);
-        return NextResponse.redirect(newUrl);
+        return NextResponse.redirect(new URL("/login", origin));
     }
 
-    // If user is authenticated and trying to access /login, redirect to /dashboard or /
     if (req.auth && pathname === "/login") {
-        const dashboardUrl = new URL("/dashboard", origin);
-        return NextResponse.redirect(dashboardUrl);
+        return NextResponse.redirect(new URL("/dashboard", origin));
     }
 
     if (pathname === "/dashboard") {
         const token = await getGitHubToken();
         if (token === "NO_TOKEN") {
-            const addTokenUrl = new URL("/", origin);
-            return NextResponse.redirect(addTokenUrl);
+            return NextResponse.redirect(new URL("/", origin));
         }
     }
 
